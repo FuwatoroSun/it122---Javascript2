517c2dd2c56f5cdae5e93e544692bb355cd93b55
FuwatoroSun <95470471+FuwatoroSun@users.noreply.github.com>
2022-04-30 16:32:16 -0700

    week2-2

--- index.js
+++ index.js
@@ -1,68 +1,8 @@
 import http from 'http';
 import fs from 'fs';
-import path from 'path';
 import { parse } from "querystring";
-import * as movie from "./data.js";
-import express from 'express';
+import {getAll, getItem} from "./data.js";
 
-const app = express();
-app.set('port', process.env.PORT || 3000);
-app.use(express.static('./public')); // set location for static files
-app.use(express.urlencoded()); //Parse URL-encoded bodies
-app.use(express.json());
-app.set('view engine', 'ejs'); // set the view engine to ejs
-
-// send static file as response
-app.get('/', (req,res) => {
-    res.type('text/html');
-    res.render('home', { movies: movie.getAll()}); // send content of 'home' view to browser
-    // res.sendFile(path.join(__dirname, './home.html'));
-});
-
-app.get('/detail', (req,res) => {
-    console.log(req.query);
-    let result = movie.getItem(req.query.title);
-    res.render("detail", {
-        title: req.query.title, 
-        result
-        }
-    );
-});
-
-/*
-app.get('/detail', (req,res) => {
-    res.type('text/html');
-    console.log(req.query);
-    res.end("Detail for " + req.query["name"]);
-    // res.sendFile(path.join(__dirname, './home.html'));
-});
-*/
-
-app.post('/detail', (req,res) => {
-    res.type('text/html');
-    console.log(req.body);
-    res.end("Detail for " + req.body["title"]);
-    // res.sendFile(path.join(__dirname, './home.html'));
-});
-
-// send plain text response
-app.get('/about', (req,res) => {
-    res.type('text/plain');
-    res.send('About page');
-});
-   
-// define 404 handler
-app.use((req,res) => {
-    res.type('text/plain');
-    res.status(404);
-    res.send('404 - Not found');
-});
-
-app.listen(app.get('port'), () => {
-    console.log('Express started');
-});
-
-/*
 function serverStatic(res, path, contentType, responseCode) {
     if(!responseCode) responseCode = 200;
     console.log(__dirname + path);
@@ -107,4 +47,3 @@ http.createServer(function(req, res){
 }).listen(process.env.PORT || 3000);
 
 console.log('after createServer');
-*/
