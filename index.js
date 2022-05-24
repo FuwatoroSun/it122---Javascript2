282f7eee6703295b154692db5ae632d60b011d08
FuwatoroSun <95470471+FuwatoroSun@users.noreply.github.com>
2022-05-23 17:24:41 -0700

    week6-2

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+import * as movie from "./data.js";
 import express from 'express';
 import { Movies } from "./Movies.js";
-import { title } from 'process';
+////////////////////////////////////////////// added this week
+import cors from 'cors';
 
 const app = express();
 app.set('port', process.env.PORT || 3000);
@@ -14,8 +16,46 @@ app.get('/', (req,res) => {
     Movies.find({}).lean()
         .then((movies) => {
         // respond to browser only after db query completes
-            // res.render('home', { movies });
-            res.render('home_react', {title: req.query.title, movies: JSON.stringify(movies)});
+            res.render('home', { movies });
+        })
+        .catch(err => next(err));
+});
+
+////////////////////////////////////////////// added this week
+app.get('/api/movies', (req,res) => {
+    Movies.find({}).lean()
+        .then((movies) => {
+        // respond to browser only after db query completes
+            res.render('home', { movies });
+        })
+        .catch(err => next(err));
+});
+
+////////////////////////////////////////////// added this week
+app.get('/api/movies/:title', (req,res) => {
+    Movies.findOne({"title": req.params.title}).lean()
+        .then((movies) => {
+            res.render('detail', { result: movies, title: req.params.title });
+        })
+        .catch(err => next(err));
+});
+
+////////////////////////////////////////////// added this week
+app.get('/api/movies/delete/:title', (req,res) => {
+    Movies.remove({"title": req.params.title}).lean()
+        .then((movies) => {
+            res.send(req.params.title + ' successfully deleted!');
+        })
+        .catch(err => next(err));
+});
+
+////////////////////////////////////////////// added this week
+////////////////////////////////////////////// 404
+app.post('/api/movies/add', (req,res) => {
+    const newmovie = {'title': req.body.title, 'year': req.body.year, 'director': req.body.director, 'genre': req.body.genre};
+    Movies.updateOne({"title": req.body.title}, newmovie, {upsert: true}).lean()
+        .then((movies) => {
+            res.send(req.body.title + ' successfully added!');
         })
         .catch(err => next(err));
 });
@@ -24,7 +64,7 @@ app.get('/detail', (req,res) => {
     // db query can use request parameters
     Movies.findOne({ title:req.query.title }).lean()
         .then((movie) => {
-            res.render('detail', {result: movie, title: req.query.title} ); //Movies.findOneの引数であるtitleと同じ形(req.query.title)にする
+            res.render('detail', {result: movie} );
         })
         .catch(err => next(err));
 });
